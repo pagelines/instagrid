c8263f947b65e955476646674a5279d0606f3521
Aleksander Hansson <aleksander.hansson@me.com>
2013-08-30 06:49:52 +0200

    1.3

--- instagrid.php
+++ instagrid.php
@@ -3,7 +3,7 @@
 	Plugin Name: InstaGrid
 	Plugin URI: http://instagrid.ahansson.com
 	Description: InstaGrid is a responsive image grid that pulls images from Instagram
-	Version: 1.2.6
+	Version: 1.3
 	Author: Aleksander Hansson
 	Author URI: http://ahansson.com
 	v3: true
